cbe13ad154482abd373d95b085e16252240491b2
Samuel Brown <samuelwbrown4@gmail.com>
2026-05-20 21:19:36 -0400

    add realtime enabled false to supabase initialization

--- db/supabase.js
+++ db/supabase.js
@@ -3,6 +3,10 @@ const {createClient} = require('@supabase/supabase-js');
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    enabled: false
+  }
+});
 
 module.exports = {supabase}
